ddf31c32a67e592adec258e7c25e32c7c1641194
NortonFire007 <rioknuto@gmail.com>
2024-12-28 21:35:01 +0200

    🎯🔧 Prevent duplicate email usage

--- account/forms.py
+++ account/forms.py
@@ -24,11 +24,26 @@ class UserRegistrationForm(forms.ModelForm):
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
+
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        # excludes the current user.This is necessary so that the user can save his current email,if it has not changed.
+        qs = User.objects.exclude(id=self.instance.id) \
+            .filter(email=data)
+        if qs.exists():
+            raise forms.ValidationError(' Email already in use.')
+        return data
 
 
 class ProfileEditForm(forms.ModelForm):
